469a80963e9b46390193250371df287c9b92144d
sadasa1340 <101466300+sadasa1340@users.noreply.github.com>
2022-03-21 16:14:34 +0900

    정규스터디/즉시스터디 리스트뷰빌더 추가
    정규스터디/즉시스터디 리스트뷰 추가했습니다.
    
    방 목록은 아직 없기에
    사진과 이름 리스트에서 불러오는 걸로 일단 구현했습니다.

--- lib/pages/immediatelyStudyPage.dart
+++ lib/pages/immediatelyStudyPage.dart
@@ -1,18 +1,132 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:study_together/config/studyTogetherColors.dart';
+import 'package:study_together/pages/studyTogether.dart';
 
 import 'makeRoomPage.dart';
 
 class ImmediatelyStudyPage extends StatelessWidget {
   const ImmediatelyStudyPage({Key? key}) : super(key: key);
 
+  /// 배경 이미지 URL
+  final String LogoImg =
+      "https://raw.githubusercontent.com/SeongjinOliver/STUDY_TOGETHER/main/Logo-removebg-preview.png";
+
+  /// 분야별 이미지 URL
+  final List<Map<String, String>> categoryList = const [
+    {
+      "name": "분야 무관",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114541-559cb125-4548-4898-b8eb-b1c4177dc5cb.png",
+    },
+    {
+      "name": "IT/개발",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114221-3b30aaab-2388-4331-b3a0-044ea6a049ad.png",
+    },
+    {
+      "name": "경제/투자",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114243-7e9ed8c8-e932-4237-a28d-cd7e2f27dbb5.png",
+    },
+    {
+      "name": "수능/공부",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114257-88f3c66c-a4b8-4d2d-9451-323a810ff9d4.png",
+    },
+    {
+      "name": "디자인",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114246-06b354b2-ab51-4ef4-8f23-7d89fc0408b0.png",
+    },
+    {
+      "name": "외국어",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114254-54c4d675-9052-4ae7-a06d-0bbb73577440.png",
+    },
+    {
+      "name": "자격증",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114249-5a18ab68-7047-4392-bc9e-8becda73c421.png",
+    },
+    {
+      "name": "창업",
+      "imgUrl":
+          "https://user-images.githubusercontent.com/101466300/159114239-c982ae3c-6129-4c0f-b757-6c734bf0257f.png",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+        title: Text(
+          '스터디 찾기',
+          style: TextStyle(
+            color: StudyTogetherColors.color1,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       body: Stack(
         children: [
-          Center(
-            child: Text("여기에 즉시 스터디 게시글 리스트를 표현해주세요."),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: ListView.builder(
+                  itemCount: categoryList.length,
+                  itemBuilder: (context, index) {
+                    final category = categoryList[index % categoryList.length];
+                    final name = category["name"] ?? "이름";
+                    final imgUrl = category["imgUrl"] ?? "";
+                    return ListTile(
+                      leading: Image.network(
+                        imgUrl,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(
+                        name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('함께 집중해서 스터디할 팀원 구합니다!'),
+                          SizedBox(
+                            width: 180,
+                            child: Row(
+                              children: [
+                                Text(
+                                  '진행중',
+                                  style: TextStyle(
+                                      color: StudyTogetherColors.color2),
+                                ),
+                                Spacer(),
+                                Text('멤버 5/10'),
+                                Spacer(),
+                                Text('온라인'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Divider()
+                        ],
+                      ),
+                      onTap: () {
+                        //launch(roompage);
+                      },
+                    );
+                  }),
+            ),
           ),
           Positioned(
             bottom: 18,
@@ -30,7 +144,7 @@ class ImmediatelyStudyPage extends StatelessWidget {
                   vertical: 10,
                 ),
                 decoration: BoxDecoration(
-                  color: Colors.grey,
+                  color: StudyTogetherColors.color4,
                   borderRadius: BorderRadius.circular(60),
                 ),
                 child: Row(
